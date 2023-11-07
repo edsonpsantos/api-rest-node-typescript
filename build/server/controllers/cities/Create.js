1b2ecca45db4a1a80c61c996252651ab71b60b18
epdsant <epdsant@gmail.com>
2023-11-07 10:12:16 +0000

    feat: Initial validation of body  with YUP and express

--- build/server/controllers/cities/Create.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.create = void 0;
-const create = (req, res) => {
-    return res.send('Create!');
-};
-exports.create = create;
--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from 'express';
+import * as yup from 'yup';
 
 interface ICities {
   name: string;
 }
 
-export const create = (req: Request<{}, {}, ICities>, res: Response) => {
-  console.log(req.body);
+const bodyValidation: yup.ObjectSchema<ICities> = yup.object().shape({
+  name: yup.string().required().min(3),
+});
 
+export const create = async (req: Request<{}, {}, ICities>, res: Response) => {
+  let validateData: ICities | undefined = undefined;
+
+  try {
+    validateData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      erros: {
+        default: yupError.message,
+      },
+    });
+  }
+
+  console.log(validateData);
   return res.send('Create!');
 };
